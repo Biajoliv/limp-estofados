58f0b2a9c0a518e78c304a69a40ff3b1d3e69209
Biajoliv <biaoliraquel92@gmail.com>
2026-09-23 19:59:27 -0300

    feat: ajustes front e banco

--- backend/src/main/java/com/limpezaestofados/backend/calculator/CalculadoraController.java
+++ backend/src/main/java/com/limpezaestofados/backend/calculator/CalculadoraController.java
@@ -1,5 +1,6 @@
 package com.limpezaestofados.backend.calculator;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
@@ -15,8 +16,15 @@ public class CalculadoraController {
     }
 
     @PostMapping
-    public ResponseEntity<BigDecimal> calcular(@RequestBody CalculoRequest request) {
-        BigDecimal valorEstimado = calculadoraService.calcular(request);
-        return ResponseEntity.ok(valorEstimado);
+    public ResponseEntity<?> calcular(@RequestBody CalculoRequest request) {
+        try {
+            BigDecimal valorEstimado = calculadoraService.calcular(request);
+            return ResponseEntity.ok(valorEstimado);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao processar o cálculo: " + e.getMessage());
+        }
     }
 }
